38d50208d4da1aae5d9d38a56218e445ded1ec7a
LizMaestracci <liztoire@gmail.com>
2022-02-01 17:33:53 +0100

    architecture react project with ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const App: FunctionComponent = () => {
+    const name: String = 'Liz';
+
+    return (
+        <h1>Bonjour {name} !</h1>
+    )
+}
+
+export default App;
