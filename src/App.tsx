2f1a97e39dcbb5ef1569047023602832c990e016
LizMaestracci <liztoire@gmail.com>
2022-02-02 12:56:41 +0100

    affichage pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemon";
 
 const App: FunctionComponent = () => {
-    const name: String = 'Liz';
+
+    // Hook d'état
+    const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+    // Hook d'effet
+    useEffect(() => {
+        setPokemon(POKEMONS);
+    }, []);
+
 
     return (
-        <h1>Bonjour {name} !</h1>
+        <div>
+            <h1 className="center">Pokédex:</h1>
+            <div className="container">
+                <div className="row">
+            <p>Il y a { pokemon.length } pokemon dans le Pokedex.</p>
+                { pokemon.map(({id, name, picture, created}) => (
+                    <div className="col s6 m4" key={id}>
+                        <div className="card horizontal">
+                            <div className="card-image">
+                                <img src={picture} alt={name} />
+                            </div>
+                        <div className="card-stacked">
+                        <div className="card-content">
+                            <p>{ name }</p>
+                            <p><small>{ created.toString() }</small></p>
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            </div>
+        </div>
+       
     )
 }
 
