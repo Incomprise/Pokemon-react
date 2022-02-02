0a3fb5f036ec0b9c381e172cef02d17862538812
LizMaestracci <liztoire@gmail.com>
2022-02-02 21:38:13 +0100

    error 404

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import { BrowserRouter as Router, Route, Link, RouteComponentProps } from "react-router-dom"
 import PokemonDetail from "./pages/pokemon-detail";
 import PokemonList from "./pages/pokemon-list";
+import PageNotFound from "./pages/page-not-found";
 
 const App: FunctionComponent = () => {
 
@@ -18,6 +19,7 @@ const App: FunctionComponent = () => {
   <Route exact path="/" component={PokemonList}/>
   <Route path="/pokemons" component={PokemonList}/>
   <Route path="/pokemons/:id" component={PokemonDetail}/>
+  <Route component={PageNotFound}/>
     </div>
 </Router>
     )
