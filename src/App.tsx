ce6f52c3abd728fdb03927af49250b02e383be05
LizMaestracci <liztoire@gmail.com>
2022-02-02 21:32:18 +0100

    routage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React, { FunctionComponent } from "react";
+import { BrowserRouter as Router, Route, Link, RouteComponentProps } from "react-router-dom"
+import PokemonDetail from "./pages/pokemon-detail";
 import PokemonList from "./pages/pokemon-list";
 
 const App: FunctionComponent = () => {
 
+    
     return (
-       <PokemonList />
+  
+<Router>
+    <div>
+        <nav>
+            <div>
+                <Link to="/">Pokedex</Link>
+            </div>
+        </nav>
+  <Route exact path="/" component={PokemonList}/>
+  <Route path="/pokemons" component={PokemonList}/>
+  <Route path="/pokemons/:id" component={PokemonDetail}/>
+    </div>
+</Router>
     )
 }
 
